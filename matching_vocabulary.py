03c479d2c59fae6432a126aa922b9870c1e637b7
Norvik Alexian <norvik@ucraft.com>
2022-05-19 17:42:32 +0400

    simple example of PhraseMatcher and viewing matches

--- matching_vocabulary.py
+++ matching_vocabulary.py
@@ -1,6 +1,7 @@
 import spacy
 
 from spacy.matcher import Matcher
+from spacy.matcher import PhraseMatcher
 
 nlp = spacy.load('en_core_web_sm')
 
@@ -43,3 +44,35 @@ matcher.add('SolarPower', [pattern1, pattern2, pattern3])
 doc2 = nlp('Solar-powered energy runs solar-powered cars.')
 
 found_matches = matcher(doc2)
+
+matcher2 = PhraseMatcher(nlp.vocab)
+
+with open('data/reaganomics.txt') as file:
+    doc3 = nlp(file.read())
+
+# create a list of match phrase
+phrase_list = ['voodoo economics', 'supply-side economics', 'trickle-down economics', 'free-market economics']
+
+# convert each phrase to a doc object
+phrase_patterns = [nlp(text) for text in phrase_list]
+
+# pass each doc object into matcher
+matcher2.add('VoodooEconomics', [*phrase_patterns])
+
+# build a list of matches
+matches = matcher2(doc3)
+
+for match_id, start, end in matches:
+    string_id = nlp.vocab.strings[match_id]
+    span = doc3[start:end]
+    print(match_id, string_id, start, end, span.text)
+
+# viewing matches
+sents = [sent for sent in doc3.sents]
+
+print(sents[0].start, sents[0].end)
+
+for sent in sents:
+    if matches[4][1] < sent.end:
+        print(sent)
+        break
