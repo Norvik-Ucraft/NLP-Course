0ced3b9e92242243460a390064515aa92b736140
Norvik Alexian <norvik@ucraft.com>
2022-05-23 18:02:29 +0400

    design new patterns and apply that patterns to recognize as named entity, simple example of named entity counting and noun chunks counting

--- named_entity_recognition.py
+++ named_entity_recognition.py
@@ -1,6 +1,7 @@
 import spacy
 
 from spacy.tokens import Span
+from spacy.matcher import PhraseMatcher
 
 nlp = spacy.load('en_core_web_sm')
 
@@ -33,3 +34,42 @@ new_ent = Span(doc3, 0, 1, label=ORG)
 doc3.ents = list(doc3.ents) + [new_ent]
 
 show_ents(doc3)
+
+doc4 = nlp('Our company plans to introduce a new vacuum cleaner.'
+           'If successful, the vacuum-cleaner will be our first product.')
+
+show_ents(doc4)
+
+# Apply the patterns to our matcher object
+phrase_list = ['vacuum cleaner', 'vacuum-cleaner']
+phrase_patterns = [nlp(text) for text in phrase_list]
+
+# Apply the patterns to our matcher object
+matcher = PhraseMatcher(nlp.vocab)
+matcher.add('newproduct', [*phrase_patterns])
+
+# Apply the matcher to our Doc object
+matches = matcher(doc4)
+
+PROD = doc.vocab.strings['PRODUCT']
+
+new_ent2 = [Span(doc4, match[1], match[2], label=PROD) for match in matches]
+
+doc4.ents = list(doc4.ents) + new_ent2
+
+show_ents(doc4)
+
+doc5 = nlp('Originally priced a $29.50, the sweater was marked down to five dollars.')
+
+show_ents(doc5)
+
+count_ent = len([ent for ent in doc5.ents if ent.label_ == 'MONEY'])
+
+doc6 = nlp('Autonomous cars shift insurance liability toward manufacturers.')
+
+for chunk in doc6.noun_chunks:
+    print(f'{chunk.text} - {chunk.root.text} - {chunk.root.dep_} - {chunk.root.head.text}')
+
+noun_chunk_count = len(list(doc6.noun_chunks))
+
+print(noun_chunk_count)
